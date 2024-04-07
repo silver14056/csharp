5ba1ee0fbc9a182f9d5b59fc0f7f9cb4266e7faa
Serg Litov <Serg@litov.com>
2024-04-07 07:29:12 +0300

    Добавили возможность ввода чисел из консоли с конвертацией из string в Int

--- seminar1/task1/Program.cs
+++ seminar1/task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите большее число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите меньшее число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondNumber * secondNumber)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
