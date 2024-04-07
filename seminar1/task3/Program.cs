0d33d76786ade6a3809d7e34a99534be5fc37573
Serg Litov <Serg@litov.com>
2024-04-07 09:29:34 +0300

    Реализовано сложение первой и последней цифры трехзначного числа

--- seminar1/task3/Program.cs
+++ seminar1/task3/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Write("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if(number > 99 && number < 1000)
+{
+    int leftDigit = number / 100;
+    int rightDigit = number % 10;
+    int result = leftDigit + rightDigit;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("Введено не трёхзначное число!");
+}
