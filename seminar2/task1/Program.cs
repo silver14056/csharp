e319047e23d4f790b4ab5d41f3aadbb892e0a094
Serg Litov <Serg@litov.com>
2024-04-07 10:16:12 +0300

    Реализовано удаление средней цифры из трёхзначного числа

--- seminar2/task1/Program.cs
+++ seminar2/task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int leftDigit = number / 100;
+    int rightDigit = number % 10;
+    int result = leftDigit * 10 + rightDigit;
+    System.Console.WriteLine(result);
+}else
+{
+    System.Console.WriteLine("Введено не трёхзначное число!");
+}
