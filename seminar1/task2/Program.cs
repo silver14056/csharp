c76ac5782ac7317263fd2240413b749f78e24c24
Serg Litov <Serg@litov.com>
2024-04-07 08:10:03 +0300

    Добавили проверку на некорректные отрицательные значения

--- seminar1/task2/Program.cs
+++ seminar1/task2/Program.cs
@@ -1,9 +1,16 @@
-﻿Console.Write("Введите число: ");
+﻿Console.Write("Введите положительное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int i = -number;
-while(i <= number)
+if (number > 0)
 {
-    System.Console.Write(i +" ");
-    i++;
+    int i = -number;
+    while (i <= number)
+    {
+        System.Console.Write(i + " ");
+        i++;
+    }
+}
+else
+{
+    System.Console.WriteLine("Вы ввели некорректное значение!");
 }
