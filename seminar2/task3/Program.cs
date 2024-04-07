140ddc190255335df471f1805d534266d82d1739
Serg Litov <Serg@litov.com>
2024-04-07 10:39:36 +0300

    Реализована проверка, кратно ли одно число другому

--- seminar2/task3/Program.cs
+++ seminar2/task3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber % secondNumber == 0)
+{
+    System.Console.WriteLine($"{firstNumber}, {secondNumber} => да");
+}
+else
+{
+    System.Console.WriteLine($"{firstNumber}, {secondNumber} => нет");
+}
