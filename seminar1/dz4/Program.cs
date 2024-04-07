14f697ba2a042b144d948377b03d923c3def2324
Serg Litov <Serg@litov.com>
2024-04-07 16:29:37 +0300

    dz4 is Done

--- seminar1/dz4/Program.cs
+++ seminar1/dz4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = 8;
+
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 0)
+    {
+        System.Console.Write(i + " ");
+    }
+}
+
+// for (int count = 2; count <= number; count += 2)
+//     {
+//         Console.Write($"{count} ");
+//     }
