89324e994d62cea1948b5c8ad6542acad184d45f
Serg Litov <Serg@litov.com>
2024-04-07 10:49:56 +0300

    Второй вариант решения, как взять третью цифру справа

--- seminar2/task4/Program.cs
+++ seminar2/task4/Program.cs
@@ -1,9 +1,15 @@
 ﻿System.Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if(number > 99)
+if (number > 99)
 {
-System.Console.WriteLine(number %1000 /100);
+    // System.Console.WriteLine(number %1000 /100);
+
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    System.Console.WriteLine(number % 10);
 }
 else
 {
