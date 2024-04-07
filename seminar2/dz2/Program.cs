9a7723bfd6dc95a7e0713dbcadf45ebc33326c10
Serg Litov <Serg@litov.com>
2024-04-07 16:54:49 +0300

    Seminar2:dz2 is Done!

--- seminar2/dz2/Program.cs
+++ seminar2/dz2/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите X: ");
+int pointX = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите Y: ");
+int pointY = Convert.ToInt32(Console.ReadLine());
+
+if (pointX != 0 && pointY != 0)
+{
+    if(pointX > 0)
+    {
+        if(pointY > 0)
+        {
+            System.Console.WriteLine("Координатная четверть №1");
+        }
+        else
+        {
+            System.Console.WriteLine("Координатная четверть №4");
+        }
+    }
+    else
+    {
+        if(pointY > 0)
+        {
+            System.Console.WriteLine("Координатная четверть №2");
+        }
+        else
+        {
+            System.Console.WriteLine("Координатная четверть №3");
+        }
+    }
+}
+else
+{
+    System.Console.WriteLine("Краями ходите товарищ!");
+}
