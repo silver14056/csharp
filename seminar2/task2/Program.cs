e5a485baa427e098500109e66a5ae93a5071d5f0
Serg Litov <Serg@litov.com>
2024-04-07 10:31:41 +0300

    Реализовали возведение второй цифры трёхзначного числа в степень равную третьей цифры

--- seminar2/task2/Program.cs
+++ seminar2/task2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int midleDigit = number / 10 % 10;
+    int rightDigit = number % 10;
+    int result = 1;
+
+    for(int i = 0; i <rightDigit; i++)
+    {
+        result *= midleDigit; 
+    }
+    System.Console.WriteLine(result);
+}else
+{
+    System.Console.WriteLine("Введено не трёхзначное число!");
+}
