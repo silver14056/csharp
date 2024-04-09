634dfd38350d04ff2ca759a45d7237c99614f256
Serg Litov <Serg@litov.com>
2024-04-09 21:23:25 +0300

    Seminar2:dz4 is Done

--- seminar2/dz4/Program.cs
+++ seminar2/dz4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int c = 1;
+    while (number / c >= 1)
+    {
+        System.Console.Write(number / c % 10 + ", ");
+        c = c * 10;
+    }
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не натуральное число!");
+}
