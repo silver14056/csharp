ed4c0f88f01e568a42a7962ef81334a2de90bbe0
Serg Litov <Serg@litov.com>
2024-04-09 20:55:27 +0300

    Seminar2:dz3 is Done

--- seminar2/dz3/Program.cs
+++ seminar2/dz3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите положительное двухзначное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10 && number <=99)
+{
+    int firstNumber = number / 10;
+    int secondNumber = number % 10;
+
+    if (firstNumber > secondNumber)
+    {
+        System.Console.WriteLine(firstNumber);
+    }
+    else
+    {
+        System.Console.WriteLine(secondNumber);
+    }
+}
+else
+{
+    System.Console.WriteLine("Вы ввели неверное число!");
+}
