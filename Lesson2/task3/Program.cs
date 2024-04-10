ec6126856eb65860b8b687808d393269bd9503ef
Serg Litov <Serg@litov.com>
2024-04-10 21:20:50 +0300

    Lesson2:task3 is Done

--- Lesson2/task3/Program.cs
+++ Lesson2/task3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int n = 10;
+int[] array = { 2, 5, 4, 7, 8, 35, 1, 3, 8, 5 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
