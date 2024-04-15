9ba2526bfcc00376350ce67f8586e2c8c3c49915
Serg Litov <Serg@litov.com>
2024-04-15 17:30:53 +0300

    Seminar3:dz4 is Done!

--- seminar3/dz4/Program.cs
+++ seminar3/dz4/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(1, 100000);
+Console.WriteLine("Сгенерированное число: " + number);
+
+
+int count = 0;
+int num = number;
+
+while (true)
+{
+    num = num / 10;
+    count++;
+    
+    if (num == 0)
+    { break; }
+    
+}
+num = number;
+
+int[] array = new int[count];
+
+for (int i = array.Length -1; i >= 0; i--)
+{
+    array[i] = num % 10;
+    num = num / 10;
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
