b6338794e9752760780301551eb915f5614ee234
Serg Litov <Serg@litov.com>
2024-04-15 11:15:56 +0300

    Seminar3:task2 is Done!

--- seminar3/task2/Program.cs
+++ seminar3/task2/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+System.Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(0, 10);
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write(array[i] + " ");
+}
+
+Console.WriteLine("Введите искомое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool numberIsFinded = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == number)
+    { 
+        numberIsFinded = true;
+        break;
+    }
+}
+
+if (numberIsFinded)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
