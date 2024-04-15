15f40eace60c137233891eb40f1c373715a3c230
Serg Litov <Serg@litov.com>
2024-04-15 10:45:13 +0300

    seminar3:task1 is Done

--- seminar3/task1/Program.cs
+++ seminar3/task1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// int[] array = new int[size];
+
+// System.Console.WriteLine("Введите элементы массива: ");
+// for (int i = 0; i < array.Length; i++)
+// {
+//     System.Console.WriteLine($"{i}-й элемент массива");
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     System.Console.Write(array[i] + " ");
+// }
+
+System.Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+Random rand = new Random();  
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(0, 10);
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write(array[i] + " ");
+}
