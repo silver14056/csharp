43cc1f611cd42876afa42c036a5be5ba3593f363
sergo book <sergo@book.ru>
2024-04-15 21:47:01 +0300

    Пробую пушить ветку Бук на ГитХаб

--- seminar4/proba/Program.cs
+++ seminar4/proba/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Начинаю работать с двух компов");
+System.Console.WriteLine("Пробую пушить ветку бук на ГитХаб и выполнить там мерге");
