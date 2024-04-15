f37bd248b1f9ea345b95c54de5c425103df96fb1
sergo book <sergo@book.ru>
2024-04-15 21:58:52 +0300

    Удалил ветку бук

--- seminar4/proba/Program.cs
+++ seminar4/proba/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Начинаю работать с двух компов");
 System.Console.WriteLine("Пробую пушить ветку бук на ГитХаб и выполнить там мерге");
+System.Console.WriteLine("Удалил ветку бук");
