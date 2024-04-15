eb1c1a66ad4d7278d3ad682e80145ea05f8176cb
Serg Litov <Serg@litov.com>
2024-04-15 12:03:58 +0300

    Seminar:task6 is Done!

--- seminar3/task6/Program.cs
+++ seminar3/task6/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// чтобы сгенерировать вещественное(double) число, нужно сложить сгенерированное целое со сгенерированным вещественным
+// потому что сгенерированное вещественное число может быть от 0 до 1
+
+Random rand = new Random();
+double number = Math.Round(rand.Next(0, 10) + rand.NextDouble(), 3);
+System.Console.WriteLine("Сгенерированное число: " + number);
