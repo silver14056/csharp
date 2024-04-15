9f23ead12b5a2dc87a5d5cc421ca7784b7bf633b
Serg Litov <Serg@litov.com>
2024-04-15 11:50:41 +0300

    Seminar3:task5 is Done!

--- seminar3/task5/Program.cs
+++ seminar3/task5/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine("Сгенерированное число " + number);
+
+int[] array = new int[3];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = number % 10;
+    number = number / 10;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
