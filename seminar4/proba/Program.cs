5b2a22b2b317e69387cf0112eefef470dc32f8dd
silver14056 <165002279+silver14056@users.noreply.github.com>
2024-04-15 22:03:10 +0300

    Изменил на хабе. тяну на бук

--- seminar4/proba/Program.cs
+++ seminar4/proba/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Начинаю работать с двух компов");
 System.Console.WriteLine("Пробую пушить ветку бук на ГитХаб и выполнить там мерге");
 System.Console.WriteLine("Удалил ветку бук");
+System.Console.WriteLine("Внес измененя в ветку майн на ХитГабе:), буду тянуть в бук");
