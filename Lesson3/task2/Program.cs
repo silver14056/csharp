bc2736ead611e5de581a3c45dd8032070f046f84
Serg Litov <Serg@litov.com>
2024-04-16 18:49:56 +0300

    Lesson3:task2 is Done

--- Lesson3/task2/Program.cs
+++ Lesson3/task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintSquares (int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i*i} ");
+        i++;
+    }
+}
+
+PrintSquares(5);
+Console.WriteLine();
+PrintSquares(10);
+Console.WriteLine();
+PrintSquares(15);
