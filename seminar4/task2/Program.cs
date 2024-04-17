bcc95e921eb406a7287a5d95d2a521ad9cd0e0f5
Serg Litov <Serg@litov.com>
2024-04-17 07:39:33 +0300

    Seminar4:task2 is Done

--- seminar4/task2/Program.cs
+++ seminar4/task2/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Main()
+{
+    int arraySize = ReadInt("Введите размер массива: ");
+    int[] array = GenerateArray(arraySize, 20, 23);
+    PrintArray(array);
+    Console.WriteLine(CountNumbers(array, 7, 1));    
+}
+
+int CountNumbers(int[] myArray, int div, int end)
+{
+    int count = 0;
+    foreach (int number in myArray)
+    {
+        if(number % div == 0 && number % 10 == end)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int size, int minRange, int maxRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; ++i)
+    {
+        tempArray[i] = rand.Next(minRange, maxRange);
+    }
+    return tempArray;
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    for (int i = 0; i < arrayForPrint.Length; ++i)
+    {
+        Console.Write(arrayForPrint[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Main();
