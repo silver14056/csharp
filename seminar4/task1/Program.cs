1381c10e8135b77c06aa3c70c6ff2f1691fb6856
Serg Litov <Serg@litov.com>
2024-04-17 07:17:44 +0300

    Seminar4:task1 is Done

--- seminar4/task1/Program.cs
+++ seminar4/task1/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//void Main()
+//{
+//    Console.Write("Введите число: ");
+//    int arraySize = Convert.ToInt32(Console.ReadLine());
+
+//    int[] array = new int[arraySize];
+//    Random rand = new Random();
+
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        array[i] = rand.Next(0, 10);
+//    }
+
+//    for (int i = 0; i < array.Length; ++i)
+//    {
+//        Console.Write(array[i] + " ");
+//    }
+//}
+
+//Main();
+
+void Main()
+{
+    int arraySize = ReadInt("Введите размер массива: ");
+    int[] array = GenerateArray(arraySize, 0, 10);
+    PrintArray(array);
+
+    int arraySize2 = ReadInt("Введите размер второго массива: ");
+    int[] array2 = GenerateArray(arraySize2, 10, 1000);
+    PrintArray(array2);
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int size, int minRange, int maxRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for(int i = 0;i < size; ++i)
+    {
+        tempArray[i] = rand.Next(minRange, maxRange);
+    }
+    return tempArray;
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    for (int i = 0; i < arrayForPrint.Length; ++i)
+    {
+        Console.Write(arrayForPrint[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Main();
