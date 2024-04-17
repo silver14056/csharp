9620ddd17fb1338a77dd922ab9ae679294fe8ecf
Serg Litov <Serg@litov.com>
2024-04-17 08:56:13 +0300

    Seminar4:task4 is Done

--- seminar4/task4/Program.cs
+++ seminar4/task4/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Main()
+{
+    int arraySize = ReadInt("Введите размер массива: ");    
+    int[] array = GenerateArray(arraySize, 0, 100);
+    PrintArray(array);
+    Console.WriteLine(CountPrime(array));
+}
+
+int CountPrime(int[] array)
+{
+    int count = 0;
+
+    foreach (int number in array)
+    {
+        if(isPrime(number))
+            count++;
+    }
+    return count;
+}
+
+bool isPrime(int number)
+{
+    if(number <2) return false;
+
+    for(int i = 2; i < Math.Sqrt(number); i++)
+    {
+        if(number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int size, int minRange, int maxRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; ++i)
+    {
+        tempArray[i] = rand.Next(minRange, maxRange + 1);
+    }
+    return tempArray;
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
+}
+
+
+Main();
+
