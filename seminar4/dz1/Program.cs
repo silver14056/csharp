9b4e21a86830e9092713dbf37958afa926915207
Serg Litov <Serg@litov.com>
2024-04-18 03:00:18 +0300

    Process seminar4:dz1

--- seminar4/dz1/Program.cs
+++ seminar4/dz1/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void main()
+{
+    //получение строки
+    //проверка на Q
+    //первевод строки в число
+    //проверка суммы на чЁтное
+    
+    
+    string stopChar = "q";
+    string stopMessage = "[STOP]";
+    string startMessage = "Вводите числа: ";
+
+    Console.WriteLine(startMessage);
+
+    
+
+    while (true)
+    {
+        
+        
+
+        if (strNumber == stopChar)
+        {
+            Console.WriteLine("[STOP]");
+            return;
+        }
+
+        int num = Convert.ToInt32(strNumber);
+        int sum = 0;
+
+        while (num != 0)
+        {
+            sum += num % 10;
+            num /= 10;
+
+            
+        }
+        Console.WriteLine(sum);
+
+        if (sum % 2 == 0)
+        {
+            Console.WriteLine("[STOP]");
+            return ;
+        }
+    }
+}
+
+
+
+string ReadString(string msg)
+{
+    return Console.ReadLine();
+}
+
+main();
