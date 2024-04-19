e3162d5ab028a40f43be65baf383cd5801a269f5
sergo book <sergo@book.ru>
2024-04-19 22:10:55 +0300

    Lesson4:task1 is Done

--- Lesson4/task1/Program.cs
+++ Lesson4/task1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] matrix = CreateMatrix(4, 6);
+ShowMatrix(matrix);
+
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        System.Console.Write($"{matrix[i, j]} ");
+    }
+    System.Console.WriteLine();
+}
+}
