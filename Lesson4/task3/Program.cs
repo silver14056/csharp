6692bf52a9bfc00fe363d437fa6000cb9ed728ce
sergo book <sergo@book.ru>
2024-04-20 06:37:58 +0300

    Lesson4:task3 is Done!

--- Lesson4/task3/Program.cs
+++ Lesson4/task3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string GetLettersFromString (string s)
+{
+    string letters = "";
+
+    foreach(char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+System.Console.WriteLine(result);
