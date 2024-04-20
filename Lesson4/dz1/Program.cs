f8e7685bd7d442110a9b63926fc3c0b719e74216
sergo book <sergo@book.ru>
2024-04-20 07:03:18 +0300

    Lesson4:dz1 is processing!

--- Lesson4/dz1/Program.cs
+++ Lesson4/dz1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string str = Console.ReadLine();
+
+string GetDigitsFromString(string s)
+{
+    string stringDigits = "";
+
+    foreach(char e in s)
+    {
+        if(char.IsAsciiDigit(e) == true)
+        {
+            stringDigits = stringDigits + e;
+        }
+    }
+    return stringDigits;
+
+}
+
+int[] StringDigitToMass(string s)
+{
+    int[] digits = new int[s.Length];
+
+    for(int i = 0; i < s.Length; i++)
+    {
+        digits[i] = s[i];
+    }
+    return digits;
+
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
+}
+
+System.Console.WriteLine(GetDigitsFromString(str));
+System.Console.WriteLine(GetDigitsFromString(str).Length);
+PrintArray(StringDigitToMass(GetDigitsFromString(str)));
