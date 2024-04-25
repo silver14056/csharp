d7900fba99f67a9bca8ed892b6ec9343d20d1fca
Serg Litov <Serg@litov.com>
2024-04-25 09:07:51 +0300

    Seminar6:task1 is Done

--- seminar6/task1/Program.cs
+++ seminar6/task1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//создать строку из символов масиива
+
+void Main()
+{
+    
+    char[] chars = { 'a', 'b', 'c',  'd' };
+
+    //1-й вариант
+    //string str = new string(chars);
+    //Console.WriteLine(str);
+
+    //2-й вариант
+    //string str = ArrayToString(chars);
+
+    //3-й вариант
+    //string str = string.Concat(str);
+
+    //4-й вариант(не сильно правильно его применять)
+    string str = string.Join("", chars);
+    Console.WriteLine(str);
+}
+
+string ArrayToString(char[] chars)
+{
+    string str = "";
+    foreach (char letter in chars)
+    {
+        str += letter;
+    }
+    return str;
+}
+
+Main();
