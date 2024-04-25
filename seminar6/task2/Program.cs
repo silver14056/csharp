46cd0df25db6284baa015474965f551df5dd645f
Serg Litov <Serg@litov.com>
2024-04-25 09:31:10 +0300

    Seminar6:task2 is Done

--- seminar6/task2/Program.cs
+++ seminar6/task2/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Из строки сделать массив символов char и вывести на экран
+
+using System.Diagnostics.SymbolStore;
+
+void Main()
+{
+    Console.WriteLine("Введите текст: ");
+    //чтобы не выводило предупреждение о том шо string может быть NULL, можно потавить вопросительный знак после типа переменной
+    string? str = Console.ReadLine();
+
+    //1-й вариант
+    //ЧТобы компилятор не ругался на возможно нулевую строку, после имени переменной можно поставить <!>
+    //char[] chars = str!.ToCharArray();
+
+    //2-й вариант
+    char[] chars = StringToCharArray(str);
+
+    //3-й вариант
+
+    PrintArray(chars);
+
+}
+
+char[] StringToCharArray(string? str)
+{
+    char[] Tempchars = new char[str!.Length];
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        Tempchars[i] = str[i];
+    }
+    return Tempchars;
+}
+
+void PrintArray(char[] chars)
+{
+    Console.WriteLine("[" + string.Join(", ", chars) + "]");
+}
+
+Main();
