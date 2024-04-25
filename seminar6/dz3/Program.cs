7a2b5703be96205e31629ce554d1ad7f74ecd4d0
Serg Litov <Serg@litov.com>
2024-04-25 12:27:50 +0300

    Seminar6:dz3 is Done!

--- seminar6/dz3/Program.cs
+++ seminar6/dz3/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 3: Задайте произвольную строку. Выясните,
+//является ли она палиндромом.
+
+void Main()
+{
+    int lengthString = ReadInt("Введите длину генерируемой строки: ");
+    string normalString = GenerateString(lengthString);
+    Console.WriteLine(normalString);
+    Console.WriteLine();
+    Console.WriteLine(isPalindrom(normalString));
+}
+
+string GenerateString(int lengthString)
+{
+    string str = "";
+    Random rnd = new Random();
+    for (int i = 0; i < lengthString; i++)
+    {
+
+        str += (char)rnd.Next(' ', '~' + 1);
+
+    }
+    return str;
+}
+
+int ReadInt(string str)
+{
+    Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string isPalindrom(string str)
+{
+    string yes = "Yes";
+    string no = "No";
+
+    for (int i = 0; i < str.Length / 2; i++)
+    {        
+        if (str[i].Equals(str[str.Length-1-i]))
+        {
+            
+        }
+        else
+        {
+            return no;
+        }
+    }
+    return yes;
+    
+}
+
+Main();
