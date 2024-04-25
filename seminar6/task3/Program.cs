72db9686678198874eb5ad98b7e9ad5ddcaa28c1
Serg Litov <Serg@litov.com>
2024-04-25 10:11:25 +0300

    Seminar6:task3 is Done

--- seminar6/task3/Program.cs
+++ seminar6/task3/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Считать строку с консол, состоящую из латинских
+//ьукв в нижнем регистре. Выяснить, сколько среди
+//введённых буквгласных.
+
+void Main()
+{
+    Console.WriteLine("Введите текст: ");
+    string? str = Console.ReadLine()?.ToLower();
+    Console.WriteLine(CountVowels(str));
+
+
+}
+
+int CountVowels(string? str)
+{
+    int count = 0;
+
+    foreach (char letter in str!)
+    {
+        if (isVowel(letter))
+            count++;
+    }
+    return count;
+}
+
+bool isVowel(char c)
+{
+    return "aeiouy".Contains(c);
+}
+
+Main();
