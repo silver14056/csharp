01c96bde76e3648d0b838224efb4cb0eeacf2a61
Serg Litov <Serg@litov.com>
2024-04-25 11:43:06 +0300

    Seminar6:dz2 is Done!

--- seminar6/dz2/Program.cs
+++ seminar6/dz2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Задайте строку, содержащую латинские буквы
+//в обоих регистрах. Сформируйте строку, в которой все
+//заглавные буквы заменены на строчные. 
+
+void Main()
+{
+    int lengthString = ReadInt("Введите длину генерируемой строки: ");
+    string normalString = GenerateString(lengthString);
+    Console.WriteLine(normalString);
+    string lowerString = normalString.ToLower();
+    Console.WriteLine(lowerString);
+}
+
+string GenerateString(int lengthString)
+{
+    string str = "";
+    Random rnd = new Random();
+    for (int i = 0; i < lengthString; i++)
+    {
+        
+            str += (char) rnd.Next(' ', '~' + 1);
+       
+    }
+    return str;
+}
+
+int ReadInt(string str)
+{
+    Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
