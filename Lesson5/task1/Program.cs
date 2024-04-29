eb44424d5c7b7286c96e8aecfece67e72479163e
Serg Litov <Serg@litov.com>
2024-04-29 23:40:30 +0300

    Lesson5 is Done

--- Lesson5/task1/Program.cs
+++ Lesson5/task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//вычисляем факториал с помощью рекурсии
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n) ;
+    int f = Fact(n - 1);
+    Console.WriteLine($"Возврат:n={n}, f={f}");
+    return n * f;
+}
+
+Console.WriteLine(Fact(2));
