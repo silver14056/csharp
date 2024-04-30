b811af368d5d8acd803a0d3267f19e99dff57878
sergo book <sergo@book.ru>
2024-04-30 09:24:33 +0300

    Seminar6:dz4 is Done

--- seminar6/dz4/Program.cs
+++ seminar6/dz4/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+void Main()
+{
+    string normalString = " one two three four five six seven eight nine ten ";
+    string reverseString = reverseWordString(normalString);
+    System.Console.WriteLine();
+    System.Console.WriteLine($"|{reverseString}|");
+}
+
+string reverseWordString(string str)
+{
+    string temp = "";
+    string rstr = "";
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if(str[i].Equals(' '))
+        {                        
+            rstr = temp + rstr;
+            rstr = str[i] + rstr;
+            temp = "";
+        }
+        else
+        {
+            temp += str[i];
+        }
+        
+    }
+    rstr = temp + rstr;
+    return rstr;
+}
+
+
+Main();
