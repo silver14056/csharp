0f05ddfa1d7b52a4c0fbfdd423de5e22a526471f
Serg Litov <Serg@litov.com>
2024-05-01 10:03:48 +0300

    Seminar7:task2 is Done

--- seminar7/task2/Program.cs
+++ seminar7/task2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая будет принимать на вход число и 
+//возвращать сумму его цифр. Использовать рекурсию
+
+void Main()
+{
+    int number = ReadInt("Введите число: ");
+    Console.WriteLine(SumOfDigits(number));
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumOfDigits(int N)
+{
+    if (N < 1) return 0;
+    return N % 10 + SumOfDigits(N / 10);
+}
+
+Main();
