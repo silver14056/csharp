d9a9b97ebc9c5150f8702da05a6f8f35b8b6f715
Serg Litov <Serg@litov.com>
2024-05-01 09:52:26 +0300

    Seminar7:task1 is Done

--- seminar7/task1/Program.cs
+++ seminar7/task1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от одного до N.
+// Использовать рекурсию! Не использовать циклы
+
+void Main()
+{
+    int number = ReadInt("Введите число: ");
+    PrintNumbers(number);
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumbers(int N)
+{
+    if (N < 1) return;
+    PrintNumbers(N - 1);
+    Console.Write(N + " ");
+    
+}
+
+Main();
